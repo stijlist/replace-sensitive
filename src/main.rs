0e09b0704b804d3ab7274b79cfe0b3d9146c7a7e
Bert Muthalaly <bert@somethingdoneright.net>
2023-02-20 14:42:03 -0500

    Remove regex crate for tokenizing.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ fn main() {
     let replacement_term = args.next().expect("no replacement term provided");
 
     let boundary_chars = ["_", "-"];
-    let boundaries_pattern = format!("[{}]", boundary_chars.join(""));
-    let boundaries = regex::Regex::new(&boundaries_pattern).unwrap();
-    let tokenized_search_term = boundaries.split(&search_term).collect::<Vec<&str>>();
-    let tokenized_replacement_term = boundaries.split(&replacement_term).collect::<Vec<&str>>();
+    let tokenized_search_term = tokenize(&search_term);
+    let tokenized_replacement_term = tokenize(&replacement_term);
 
     let mut search_terms = vec![search_term.clone()];
     let mut replacement_terms = vec![replacement_term.clone()];
